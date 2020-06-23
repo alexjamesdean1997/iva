e2f095ad510f604c219c0f804316864026bd5e13
Maël Querré <mael.querre@gmail.com>
2020-06-23 10:20:30 +0200

    Clean tests

--- src/index.js
+++ src/index.js
@@ -3,12 +3,5 @@ import './css/main.css'
 import ScrollMagic from 'scrollmagic'
 
 window.addEventListener('DOMContentLoaded', () => {
-  const controller = new ScrollMagic.Controller()
 
-  const scene = new ScrollMagic.Scene({
-    triggerElement: '.title', // starting scene, when reaching this element
-    duration: 400 // pin the element for a total of 400px
-  }).setPin('.title') // the element we want to pin
-
-  controller.addScene(scene)
 })
