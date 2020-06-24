5f1d4c9a8d3a944a0c6f936ac3637906b3adecf5
Maël Querré <mael.querre@gmail.com>
2020-06-24 11:38:00 +0200

    Format code

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,45 +1,33 @@
 import ScrollMagic from 'scrollmagic'
-import $ from 'jquery';
+import $ from 'jquery'
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello')
-});
 
-$(document).ready(function(){
+  // Hero animation
+  $('.hero-title').animate({ 'opacity': '1' }, 2000)
 
-  //hero animation
-  $('.hero-title').animate({'opacity':'1'},2000);
+  setTimeout(function () {
+    $('.hand-left').addClass('slidein')
+    $('.hand-right').addClass('slidein')
 
-  setTimeout(function(){
-
-    $('.hand-left').addClass('slidein');
-    $('.hand-right').addClass('slidein');
-
-  }, 1000);
-
-});
+  }, 1000)
+})
 
 
 function scrollVideo(video, difference) {
-
-  var videoLength = video.duration;
-  video.currentTime = (difference / ($(document).height() - $(window).height())) * videoLength;
-
+  const videoLength = video.duration
+  video.currentTime = (difference / ($(document).height() - $(window).height())) * videoLength * 3
 }
 
 
-$(window).scroll( function(){
+$(window).scroll(function () {
+  $('.video').each(function () {
+    const objectBottom = $(this).position().top
+    const windowBottom = $(window).scrollTop() + $(window).height()
+    const difference = windowBottom - objectBottom
 
-  $('.video').each( function(i){
-
-    let bottom_of_object = $(this).position().top;
-    let bottom_of_window = $(window).scrollTop() + $(window).height();
-    let difference = bottom_of_window - bottom_of_object;
-
-    if( bottom_of_window > bottom_of_object ){
-      scrollVideo(this, difference);
+    if (windowBottom > objectBottom) {
+      scrollVideo(this, difference)
     }
-
-  });
-
-});
+  })
+})
