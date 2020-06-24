837a37f3fbdf6aadd3bc30f3e587b91ce02a20b2
Alex Dean <alex.dean9g@gmail.com>
2020-06-24 11:26:46 +0200

    scroll based video play

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -18,3 +18,28 @@ $(document).ready(function(){
   }, 1000);
 
 });
+
+
+function scrollVideo(video, difference) {
+
+  var videoLength = video.duration;
+  video.currentTime = (difference / ($(document).height() - $(window).height())) * videoLength;
+
+}
+
+
+$(window).scroll( function(){
+
+  $('.video').each( function(i){
+
+    let bottom_of_object = $(this).position().top;
+    let bottom_of_window = $(window).scrollTop() + $(window).height();
+    let difference = bottom_of_window - bottom_of_object;
+
+    if( bottom_of_window > bottom_of_object ){
+      scrollVideo(this, difference);
+    }
+
+  });
+
+});
