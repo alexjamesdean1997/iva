9a972d895c75b87f88be9ab677e98160fe227474
Alex Dean <alex.dean9g@gmail.com>
2020-06-24 10:00:03 +0200

    hero effect on load

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,20 @@
 import ScrollMagic from 'scrollmagic'
+import $ from 'jquery';
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log('Hello')
-})
+});
+
+$(document).ready(function(){
+
+  //hero animation
+  $('.hero-title').animate({'opacity':'1'},2000);
+
+  setTimeout(function(){
+
+    $('.hand-left').addClass('slidein');
+    $('.hand-right').addClass('slidein');
+
+  }, 1000);
+
+});
